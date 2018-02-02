99234c9fb199db356dffe516cad8e143090b55cf
dailxx <zhangzhiheng456@gmail.com>
2018-02-02 23:17:01 +0800

    '新增HTML静态模板文件'

---
+++ server/template/normal.js
@@ -0,0 +1,26 @@
+module.exports = `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>Koa Server HTML</title>
+      <link href="https://cdn.bootcss.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
+      <script src="https://cdn.bootcss.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js"></script>
+      <script src="https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js"></script>
+    </head>
+    <body>
+      <div class="cotainer">
+        <div class="row">
+          <div class="col-md-8">
+            <h1>Hi Luck</h1>
+            <p>This is Scott</p>
+          </div>
+          <div class="col-md-4">
+            <p>测试静态HTMl页面</p>
+          </div>
+        </div>
+      </div>
+    </body>
+  <html>
+`
